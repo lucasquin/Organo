ce6b3157eecaf8feecbc657dd1531b35e9289c9e
Lucas Lopes <lucasquin@protonmail.com>
2022-09-12 08:54:45 -0300

    use state

--- src/App.js
+++ src/App.js
@@ -1,32 +1,54 @@
+import { useState } from 'react';
 import './App.css';
 
-const atividade = [
+// crio um objeto para usar no initialState
+let initialState = [
   {
-      "id": 1,
-      "descricao": "Primeira sessão"
+    "id": 1,
+    "descricao": "Primeira sessão"
   },
   {
     "id": 2,
     "descricao": "Segunda sessão"
-  },
-  {
-    "id": 3,
-    "descricao": "Primeira sessão"
-  },
-  {
-    "id": 4,
-    "descricao": "Segunda sessão"
   }
 ]
 
+
 function App() {
+
+  // para usar o useState ele tem que ser importado
+  const [atividades, setAtividades] = useState(initialState);
+
+  function addAtividade(e) {
+
+    // impede que o navegador atualize a pagina quando clicar no botao
+    e.preventDefault();
+  
+    // preenchendo o objeto
+    const atividade = {
+      "id": document.getElementById("id").value,
+      "descricao": document.getElementById("descricao").value,
+    };
+
+    // spread operator, cria um novo array de atividades e faz o push de atividade para esse novo array.
+    setAtividades([...atividades,  {...atividade}]);
+  }
+
   return (
-    <div className="mt-3">
-        <ul className="list-group"> {atividade.map(x => (
-              <li key={x.id} className="list-group-item">{x.id} - {x.descricao}</li>
-          ))}        
-        </ul>
-    </div>
+    <>
+      <form>  
+        <input id="id" type="text" placeholder='id'/>
+        <input id="descricao" type="text" placeholder='descricao'/>
+        <button onClick={addAtividade}>+ Atividade</button>
+      </form>
+      
+      <div className="mt-3">
+          <ul className="list-group"> {atividades.map(x => (
+                <li key={x.id} className="list-group-item">{x.id} - {x.descricao}</li>
+            ))}        
+          </ul>
+      </div>
+    </>
   );
 }
 
