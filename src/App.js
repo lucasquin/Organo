be73e57c082c860a9d891444b4261e17223eaaa4
Lucas Lopes <lucasquin@protonmail.com>
2022-09-11 22:36:49 -0300

    lista de atividade adionado

--- src/App.js
+++ src/App.js
@@ -4,16 +4,27 @@ const atividade = [
   {
       "id": 1,
       "descricao": "Primeira sessão"
+  },
+  {
+    "id": 2,
+    "descricao": "Segunda sessão"
+  },
+  {
+    "id": 3,
+    "descricao": "Primeira sessão"
+  },
+  {
+    "id": 4,
+    "descricao": "Segunda sessão"
   }
 ]
+
 function App() {
   return (
     <div className="mt-3">
-        <ul className="list-group">
-          <li className="list-group-item">{atividade[0].id} {atividade[0].descricao}</li>
-          <li className="list-group-item" onClick={() => ( console.log("JSX"))}>Segunda sessão</li>
-          <li className="list-group-item">Terceira sessão</li>
-          <li className="list-group-item">Quarta sessão</li>
+        <ul className="list-group"> {atividade.map(x => (
+              <li key={x.id} className="list-group-item">{x.id} - {x.descricao}</li>
+          ))}        
         </ul>
     </div>
   );
