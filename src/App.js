b4b476eff4b806de9678fd5388e07bcc9f4084f4
Lucas Lopes <lucasquin@protonmail.com>
2022-09-20 20:48:13 -0300

    Alteração do projeto

--- src/App.js
+++ src/App.js
@@ -1,64 +1,24 @@
-import { useState } from 'react';
+import logo from './logo.svg';
 import './App.css';
 
-// crio um objeto para usar no initialState
-let initialState = [
-  {
-    "id": 1,
-    "descricao": "Primeira sessão"
-  },
-  {
-    "id": 2,
-    "descricao": "Segunda sessão"
-  }
-]
-
-
 function App() {
-
-  // para usar o useState ele tem que ser importado
-  const [atividades, setAtividades] = useState(initialState);
-
-  function addAtividade(e) {
-
-    // impede que o navegador atualize a pagina quando clicar no botao
-    e.preventDefault();
-  
-    // preenchendo o objeto
-    const atividade = {
-      "id": document.getElementById("id").value,
-      "descricao": document.getElementById("descricao").value,
-    };
-
-    // spread operator, cria um novo array de atividades e faz o push de atividade para esse novo array.
-    // deve ser criado um novo array de atividades, se não é criado um array dentro de outro array
-    setAtividades([...atividades,  {...atividade}]);
-  }
-
   return (
-    <>
-    <form className='row g-3'>
-      <div className="col-md-6">
-          <label htmlFor="inputEmail4">Id</label>
-          <input id="id" type="text" className="form-control" placeholder="Id" />
-      </div>
-      <div className="col-md-6">
-        <label htmlFor="inputEmail4"> Descrição</label>
-        <input id="descricao" type="text" className="form-control" placeholder="Descrição" />
-      </div>
-      <hr />
-      <div className="col-12">
-        <button className="btn btn-outline-success" onClick={addAtividade}>+ Atividade</button>
-      </div>
-    </form>
-
-    <div className="mt-3">
-      <ul className="list-group"> {atividades.map(x => (
-            <li key={x.id} className="list-group-item">{x.id} - {x.descricao}</li>
-        ))}        
-      </ul>
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Editar aqui <code>src/App.js</code> e salvar para recarregar .
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
     </div>
-    </>
   );
 }
 
