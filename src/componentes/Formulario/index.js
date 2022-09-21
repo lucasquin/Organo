709288152c78d728b93910f045a04ca5e375cf83
Lucas Lopes <lucasquin@protonmail.com>
2022-09-21 08:50:34 -0300

    Mudança de estado do input

--- src/componentes/Formulario/index.js
+++ src/componentes/Formulario/index.js
@@ -15,14 +15,18 @@ const Formulario = () => {
         "Inovação e Gestão"
     ]
 
+    const aoSalvar = (e) => {
+        e.preventDefault()
+        console.log("Card criado!")
+    }
     return (
         <section className="formulario">
-            <form>
+            <form onSubmit={aoSalvar}>
                 <h2>Preencha os dados para criar o card do colaborador.</h2>
-                <CampoTexto label="Nome" placeholder="Digite seu nome"/>
-                <CampoTexto label="Cargo" placeholder="Digite seu cargo"/>
+                <CampoTexto obrigatorio={true} label="Nome" placeholder="Digite seu nome"/>
+                <CampoTexto obrigatorio={true} label="Cargo" placeholder="Digite seu cargo"/>
                 <CampoTexto label="Imagem" placeholder="Informe o endereço da imagem"/>
-                <ListaSuspensa label="Time" itens={times}/>
+                <ListaSuspensa obrigatorio={true} label="Time" itens={times}/>
                 <Botao>Criar card</Botao>
             </form>
         </section>
