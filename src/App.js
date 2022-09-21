09b9989145b2f3134634d44604554a50a18ee4d2
Lucas Lopes <lucasquin@protonmail.com>
2022-09-20 22:24:36 -0300

    Componente de campo de texto criado

--- src/App.js
+++ src/App.js
@@ -1,9 +1,13 @@
 import Banner from './componentes/Banner/Banner';
+import CampoTexto from './componentes/CampoTexto/CampoTexto';
 
 function App() {
   return (
     <div className="App">
        <Banner />
+       <CampoTexto label="Nome" placeholder="Digite seu nome"/>
+       <CampoTexto label="Cargo" placeholder="Digite seu cargo"/>
+       <CampoTexto label="Imagem" placeholder="Informe o endereço da imagem"/>
     </div>
   );
 }
